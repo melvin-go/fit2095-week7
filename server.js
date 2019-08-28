12bad76c533c0b97f18b4602cb06ef5d6588d46b
melvin-go <melvin.j.g0@gmail.com>
2019-08-28 21:02:58 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+
+let app = express();
+
+let taskList = [];
+
+//app.use(express.static("views"));
+app.use(express.static("img"));
+let filePath = __dirname + '/views/';
+
+// Setup view engine
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Parse post request
+app.use(bodyParser.urlencoded({
+    extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.sendFile(filePath + 'index.html');
+});
+
+app.get('/addTask', function(req, res) {
+    res.sendFile(filePath + 'addTask.html');
+});
+
+app.post('/addTask', function(req, res) {
+    taskList.push({
+        taskName: req.body.taskName,
+        taskDue: req.body.taskDue,
+        taskDesc: req.body.taskDesc
+    })
+    res.send('New Task Added!!');
+    console.log(taskList);
+});
+
+app.get('/listAllTasks', function(req, res){
+    res.render('listTasks.html', {list: taskList});
+});
+
+app.listen(8080);
